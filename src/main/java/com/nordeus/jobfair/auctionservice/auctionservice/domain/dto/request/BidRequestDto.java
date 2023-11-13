97efc398749825620e2ca19aeaf638ebee3cc550
Milan Jovanovic <milanj1705@gmail.com>
2023-11-13 01:29:20 +0100

    Implement bid endpoint

---
+++ src/main/java/com/nordeus/jobfair/auctionservice/auctionservice/domain/dto/request/BidRequestDto.java
@@ -0,0 +1,12 @@
+package com.nordeus.jobfair.auctionservice.auctionservice.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BidRequestDto {
+    private String userId;
+}
